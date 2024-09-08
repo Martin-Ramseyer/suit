228ca8b7143921c0d93841d2fd93e47288f52a0e
martin-ramseyer <martinramseyer4@gmail.com>
2024-09-08 00:42:19 -0300

    Inicio con flayer estilado.

---
+++ suitMvc/Controllers/FlayerController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using suitMvc.Data;
+
+namespace suitMvc.Controllers
+{
+    public class FlayerController : Controller
+    {
+        private readonly SuitDbContext _context;
+        public FlayerController(SuitDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Flayer
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
